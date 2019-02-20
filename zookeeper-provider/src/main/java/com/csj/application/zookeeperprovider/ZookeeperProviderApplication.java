e3c3cd4d4e7786d621a40a1bd9b46b3bd6e8c991
yew325 <yew325@csjscm.com>
2019-02-20 18:07:55 +0800

    111

--- zookeeper-provider/src/main/java/com/csj/application/zookeeperprovider/ZookeeperProviderApplication.java
+++ zookeeper-provider/src/main/java/com/csj/application/zookeeperprovider/ZookeeperProviderApplication.java
@@ -2,12 +2,22 @@ package com.csj.application.zookeeperprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZookeeperProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZookeeperProviderApplication.class, args);
-	}
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperProviderApplication.class, args);
+    }
+
 
 }
