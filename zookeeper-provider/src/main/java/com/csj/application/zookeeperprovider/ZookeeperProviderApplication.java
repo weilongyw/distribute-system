f49c23c6af2afe0a96b6d96552a6f2155679f653
yew325 <yew325@csjscm.com>
2019-02-21 13:30:58 +0800

    111

--- zookeeper-provider/src/main/java/com/csj/application/zookeeperprovider/ZookeeperProviderApplication.java
+++ zookeeper-provider/src/main/java/com/csj/application/zookeeperprovider/ZookeeperProviderApplication.java
@@ -1,8 +1,11 @@
 package com.csj.application.zookeeperprovider;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -10,10 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ZookeeperProviderApplication {
 
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello world";
+    @Autowired
+    private DiscoveryClient discovery;
+
+    @Autowired(required = false)
+    private Registration registration;
+
+
+    @RequestMapping("/hi")
+    public String hi() {
+        return "Hello World! from " + this.registration;
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ZookeeperProviderApplication.class, args);
